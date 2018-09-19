0d849753bee6aa38c6bb3d985a18c98f8a4b76ea
joyonto51 <meghla1609>
2018-09-19 07:40:39 +0600

    requirements.txt file added.

--- snippets/views.py
+++ snippets/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse, JsonResponse
+
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
